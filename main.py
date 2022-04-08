ad8852f114e663cc3cb5905ac32e8f1b3ec9aafe
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-08 17:43:36 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 x = 1
 y = 0
 display = 5
-while True:
-    for i in range(display-1):
-        basic.clear_screen()
-        led.plot(x, y)
-        led.plot(x-1, y+1)
-        basic.pause(1000)
-        x += 1
-        y += 1
-    x = 1
-    y = 0
+led.plot(x, y)
+led.plot(x-1, y+1)
+def on_button_pressed_ab():
+    while True:
+        x = 1
+        y = 0
+        for i in range(display-1):
+            basic.clear_screen()
+            led.plot(x, y)
+            led.plot(x-1, y+1)
+            basic.pause(3000)
+            x += 1
+            y += 1
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 let x = 1
 let y = 0
 let display = 5
-while (true) {
-    for (let i = 0; i < display - 1; i++) {
-        basic.clearScreen()
-        led.plot(x, y)
-        led.plot(x - 1, y + 1)
-        basic.pause(1000)
-        x += 1
-        y += 1
+led.plot(x, y)
+led.plot(x - 1, y + 1)
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let x: number;
+    let y: number;
+    while (true) {
+        x = 1
+        y = 0
+        for (let i = 0; i < display - 1; i++) {
+            basic.clearScreen()
+            led.plot(x, y)
+            led.plot(x - 1, y + 1)
+            basic.pause(3000)
+            x += 1
+            y += 1
+        }
     }
-    x = 1
-    y = 0
-}
+})
